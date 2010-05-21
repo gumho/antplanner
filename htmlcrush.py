9244d77b13093ace7e8dca4273557088b34b8323
Ryan Hsu <rchsu@uci.edu>
2010-05-21 00:03:19 -0700

    initial commit

---
+++ htmlcrush.py
@@ -0,0 +1,15 @@
+from BeautifulSoup import BeautifulSoup
+
+#consider caching if gets too expensive
+
+def strip_search(html):
+	form_html = BeautifulSoup(html).find('form', action='http://websoc.reg.uci.edu/')
+	form_html['action'] = '/schedules'
+	return str(form_html)
+	
+def strip_schedule(html):
+	schedule_html = BeautifulSoup(html).find('div', 'course-list')
+	if schedule_html is None:
+		return "Not a valid selection"
+	else:
+		return str(schedule_html)
