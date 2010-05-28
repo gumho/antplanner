1e64946e2edd6082b4daf39fc8e9606b1c07916a
Ryan Hsu <ryanlives@gmail.com>
2010-05-27 18:35:59 -0700

    only one submit button on search page now

--- scraper.py
+++ scraper.py
@@ -1,10 +1,16 @@
 from lib.BeautifulSoup import BeautifulSoup
 
-#consider caching if gets too expensive
-
 def strip_search(html):
 	form_html = BeautifulSoup(html).find('form', action='http://websoc.reg.uci.edu/')
+	
+	#replace form submit with our own link
 	form_html['action'] = '/schedules'
+	
+	#remove 'Display Text Results' button
+	text_buttons = form_html.findAll(attrs={"class" : "banner-width"})
+	for i in text_buttons:
+		i.replaceWith('<p id=\"submit-container\"><input type="submit" value="Display Web Results" name="Submit"></p>')
+	
 	return str(form_html)
 	
 def strip_schedule(html):
