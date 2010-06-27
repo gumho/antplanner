93dcb2e82f03330d4287406296e310a4a58403c0
Ryan Hsu <ryanlives@gmail.com>
2010-06-26 21:50:07 -0700

    separated auth classes into auth.py

--- antplanner.py
+++ antplanner.py
@@ -91,39 +91,6 @@ class schedules:
 				schedule_page = "UCI webpage is not available at the moment"
 		
 		return render.schedule(schedule_page)
-				
-class admin:
-	@requireAdmin
-	def GET(self):		
-		stats = memcache.get_stats()
-		version = memcache.get('websoc-version')
-		if version is None:
-			version = ''
-		
-		logout_link = users.get_current_user()
-		return render.admin(stats, version, logout_link)
-
-class adminFlushCache:
-	@requireAdmin
-	def POST(self):		
-		if memcache.flush_all():
-			message = "Cache successfully flushed."
-		else:
-			message = "Cache couldn't be flushed. Server or RPC error."
-			
-		raise web.seeother('/admin')
-
-class latestWebSoc:
-	@requireAdmin
-	def POST(self):
-		try:
-			page = urlfetch.fetch("http://websoc.reg.uci.edu")
-			version = scraper.strip_websoc_version(page.content)
-			memcache.add('websoc-version', version, 60 * 60 * 6)
-		except urlfetch.Error:
-			memcache.add('websoc-version', 'websoc down?', 30)
-		
-		raise web.seeother('/admin')
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
--- auth.py
+++ auth.py
@@ -1,5 +1,12 @@
 import web
+
+import scraper
+
 from google.appengine.api import users
+from google.appengine.api import memcache
+from google.appengine.api import urlfetch 
+
+render = web.template.render('templates/')
 
 def requireAdmin(f):
 	def wrapper(*args, **kw):
@@ -12,3 +19,37 @@ def requireAdmin(f):
 			return web.seeother(users.create_login_url('/admin'))
 	
 	return wrapper
+
+class admin:
+	@requireAdmin
+	def GET(self):		
+		stats = memcache.get_stats()
+		version = memcache.get('websoc-version')
+		if version is None:
+			version = ''
+
+		logout_link = users.create_logout_url('/admin')
+		
+		return render.admin(stats, version, logout_link)
+
+class adminFlushCache:
+	@requireAdmin
+	def POST(self):		
+		if memcache.flush_all():
+			message = "Cache successfully flushed."
+		else:
+			message = "Cache couldn't be flushed. Server or RPC error."
+
+		raise web.seeother('/admin')
+
+class latestWebSoc:
+	@requireAdmin
+	def POST(self):
+		try:
+			page = urlfetch.fetch("http://websoc.reg.uci.edu")
+			version = scraper.strip_websoc_version(page.content)
+			memcache.add('websoc-version', version, 60 * 60 * 6)
+		except urlfetch.Error:
+			memcache.add('websoc-version', 'websoc down?', 30)
+
+		raise web.seeother('/admin')
