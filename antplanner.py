a5e35560ec1e8e0673d2384bae559ada761389e9
Ryan Hsu <ryanlives@gmail.com>
2010-10-16 23:20:38 -0700

    added frontend login/logout functionality

--- antplanner.py
+++ antplanner.py
@@ -23,14 +23,14 @@ render = web.template.render('templates/')
 
 class index:
 	def GET(self):
-		# user = users.get_current_user()
-		# if user:
-		# 	html = "Logged in as %s | <a href='%s'>Logout</a>" % (user.email(), users.create_logout_url('/'))
-		# else: #not logged in
-		# 	html = "<a href='%s'>Login</a>" % users.create_login_url("/")
-		# 	
-		# login_div = "<p id=\"account\">%s</p>" % html
-		return render.index()
+		user = users.get_current_user()
+		if user:
+			html = "Logged in as %s | <a href='%s'>Logout</a>" % (user.email(), users.create_logout_url('/'))
+		else: #not logged in
+			html = "<a href='%s'>Login</a>" % users.create_login_url("/")
+			
+		login_div = "<p id=\"account\">%s</p>" % html
+		return render.index(login_div)
 
 class search:
 	def GET(self):
