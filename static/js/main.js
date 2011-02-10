0193f9770c296a20437225120135243a50e13980
Ryan Hsu <ryanlives@gmail.com>
2011-02-09 21:47:23 -0800

    refactor stage 2

--- static/js/main.js
+++ static/js/main.js
@@ -53,15 +53,66 @@ Utils = function() {
 
 
 CourseManager = function() {
-	this.course_bag = []
-	this.doShit = function() {
-		this.course_bag = ['aoeu'];
-		alert(this.course_bag);
+	//courseBag contains calEvents
+	this.courseBag = []
+	
+	this.clearCourseBag = function() {
+		this.courseBag = [];
+	}
+	
+	this.addToBag = function(calEvent) {
+		this.courseBag.push(calEvent);
 	}
 }
 
 SOCParser = function() {
 	
+}
+
+APCalendar = function(courseManager) {
+	this.initCalendar = function() {
+		$('#calendar').weekCalendar({
+			readonly: true,
+			timeslotsPerHour: 4,
+			useShortDayNames: true,
+			showHeaderDate: false,
+			firstDayOfWeek: 1,
+			daysToShow:5,
+			businessHours: {start: 7, end: 23, limitDisplay: true},
+			allowCalEventOverlap: true,
+			overlapEventsSeparate: true,
+			buttons: false,
+			defaultEventLength: 0,
+			height: function($calendar){
+				return getCorrectLowerHeights();
+			},
+			draggable : function(calEvent, element) {	return false; },
+			resizable : function(calEvent, element) {	return false; },
+			eventClick : function(calEvent, element) {		
+				//handler for removing courses		
+				var bag = courseManager.courseBag
+				
+				for(i in bag) {
+					if(calEvent.title == bag[i].title) {
+						var removalID = bag[i].id;
+						$('#calendar').weekCalendar('removeEvent', removalID);
+						delete bag[i];
+					}
+				}
+				//
+				//TODO: clean up undefined after 'deletes'
+				//
+			}
+		});
+		
+		//switch calendar view to the static date, rather than today's view
+		$('#calendar').weekCalendar('gotoWeek',  new Date(START_YEAR, START_MONTH, START_DAY));
+		
+	}
+	this.clearAllEvents = function() {
+		$('#calendar').weekCalendar('clear');
+		courseManager.clearCourseBag();
+	}
 }
 
 function isDuplicateCourse(courseString) {
@@ -71,11 +122,6 @@ function isDuplicateCourse(courseString) {
 		}
 	}
 	return false;
-}
-
-function clearAllEvents() {
-	jQuery('#calendar').weekCalendar('clear');
-	COURSE_BAG = [];
 }
 
 //parses course string into readable format ie. INF 41 Software Reqs
@@ -175,10 +221,10 @@ function createEvents(courseName, courseDates, courseTime) {
 
 function getCorrectLowerHeights() {
 	var heights = function() {
-		if(jQuery('#header').is(":visible")) {
-			return jQuery(window).height() - jQuery('#header').outerHeight() - jQuery('#controls').outerHeight();
+		if($('#header').is(":visible")) {
+			return $(window).height() - $('#header').outerHeight() - $('#controls').outerHeight();
 		} else {
-			return jQuery(window).height() - jQuery('#controls').outerHeight();
+			return $(window).height() - $('#controls').outerHeight();
 		}
 	}
 	
@@ -187,70 +233,58 @@ function getCorrectLowerHeights() {
 
 function resizeLowerContent() {
 	var heights = getCorrectLowerHeights();
-	jQuery('#school').css('height', heights);
-	jQuery('#calendar').weekCalendar('resizeCalendar', heights);
+	$('#school').css('height', heights);
+	$('#calendar').weekCalendar('resizeCalendar', heights);
 }
 
-jQuery(document).ready(function() {
+WindowManager = function() {
+	this.getCorrectBottomSectionHeight = function() {
+		var heights = function() {
+			if($('#header').is(":visible")) {
+				return $(window).height() - $('#header').outerHeight() - $('#controls').outerHeight();
+			} else {
+				return $(window).height() - $('#controls').outerHeight();
+			}
+		}
+
+		return heights();
+	},
+	this.resizeBottomSectionHeight = function() {
+		var heights = this.getCorrectBottomSectionHeight();
+		$('#school').css('height', heights);
+		$('#calendar').weekCalendar('resizeCalendar', heights);
+	}
+}
+
+$(document).ready(function() {
 	var courseManager = new CourseManager();
+	var socParser = new SOCParser();
+	var apCalendar = new APCalendar(courseManager);
+	var windowManager = new WindowManager();
 	
-	jQuery('#school').css('height', getCorrectLowerHeights());
+	$('#school').css('height', windowManager.getCorrectBottomSectionHeight());
 	
 	//initialize calendar
-	jQuery('#calendar').weekCalendar({
-		readonly: true,
-		timeslotsPerHour: 4,
-		useShortDayNames: true,
-		showHeaderDate: false,
-		firstDayOfWeek: 1,
-		daysToShow:5,
-		businessHours: {start: 7, end: 23, limitDisplay: true},
-		allowCalEventOverlap: true,
-		overlapEventsSeparate: true,
-		buttons: false,
-		defaultEventLength: 0,
-		height: function(jQuerycalendar){
-			return getCorrectLowerHeights();
-		},
-		draggable : function(calEvent, element) {	return false; },
-		resizable : function(calEvent, element) {	return false; },
-		eventClick : function(calEvent, element) {		
-			//handler for removing courses						
-			for(i in COURSE_BAG) {
-				if(calEvent.title == COURSE_BAG[i].title) {
-					var removalID = COURSE_BAG[i].id;
-					
-					jQuery('#calendar').weekCalendar('removeEvent', removalID);
-					delete COURSE_BAG[i];
-				}
-			}
-			//
-			//TODO: clean up undefined after 'deletes'
-			//
-		}
-	});
-	
-	//switch calendar view to the static date, rather than today's view
-	jQuery('#calendar').weekCalendar('gotoWeek',  new Date(START_YEAR, START_MONTH, START_DAY));
-	
+	apCalendar.initCalendar();
+
 	//school handlers
-	jQuery('#school').load(function() {
-		var list = jQuery('.course-list', frames['school'].document);
+	$('#school').load(function() {
+		var list = $('.course-list', frames['school'].document);
 		
 		//hover over valid course
-		jQuery("tr[valign*='top']", list).hover(
+		$("tr[valign*='top']", list).hover(
 			function() {
-				jQuery(this).css({'color': 'red', 'cursor': 'pointer'});
+				$(this).css({'color': 'red', 'cursor': 'pointer'});
 			},
 			function() {
-				jQuery(this).css({'color': 'black', 'cursor': 'default'});
+				$(this).css({'color': 'black', 'cursor': 'default'});
 			}
 		);
 		
 		//click on course
-		jQuery("tr[valign*='top']", list).click(function() {
+		$("tr[valign*='top']", list).click(function() {
 
-			timeString = jQuery(this).find('td').eq(SCHEDULE_TIME_INDEX).html();
+			timeString = $(this).find('td').eq(SCHEDULE_TIME_INDEX).html();
 			
 			if(timeString == null) {
 				broadcastMessage("You didn't click on a course.");
@@ -263,9 +297,9 @@ jQuery(document).ready(function() {
 			}
 			
 			//parse for course information
-			var courseCode = jQuery(this).find('td').eq(SCHEDULE_CODE_INDEX).html();
-			var courseType = jQuery(this).find('td').eq(SCHEDULE_TYPE_INDEX).html();
-			var courseString = jQuery(this).prevAll().find('.CourseTitle:last').html();
+			var courseCode = $(this).find('td').eq(SCHEDULE_CODE_INDEX).html();
+			var courseType = $(this).find('td').eq(SCHEDULE_TYPE_INDEX).html();
+			var courseString = $(this).prevAll().find('.CourseTitle:last').html();
 			
 			var courseName = constructCourseName(courseString, courseType, courseCode);
 			if(isDuplicateCourse(courseName)) {
@@ -275,7 +309,7 @@ jQuery(document).ready(function() {
 			
 			var courseTime = getCourseTime(timeString);
 			//scroll calendar viewport to expect newly added course events
-			jQuery('#calendar').weekCalendar('scrollToHour', courseTime.startHour);
+			$('#calendar').weekCalendar('scrollToHour', courseTime.startHour);
 			
 			//create the array of cal events
 			var calEvents = createEvents(
@@ -295,36 +329,37 @@ jQuery(document).ready(function() {
 				calEvents[i].borderColor = colorPairing.borderColor;
 
 				//create the events
-				jQuery('#calendar').weekCalendar('updateEvent', calEvents[i]);
+				$('#calendar').weekCalendar('updateEvent', calEvents[i]);
 				
 				//add course to the master list of courses
-				COURSE_BAG.push(calEvents[i]);
+				courseManager.addToBag(calEvents[i]);
+				alert(courseManager.courseBag);
 			}
 		});
 	});
 	
 	//other event handlers
-	jQuery(window).resize(function() {
-		resizeLowerContent();
+	$(window).resize(function() {
+		windowManager.resizeBottomSectionHeight();
 	});
 		
-	jQuery('a#clear-calendar').click(function() {
-		clearAllEvents();
+	$('a#clear-calendar').click(function() {
+		courseManager.clearCourseBag();
 		return false;
 	});
 	
-	jQuery('a#pull-tab').toggle( function() {
-			jQuery(this).html('More');
-			var newHeight = jQuery(window).height() - jQuery('#controls').outerHeight();
-			jQuery('#header').slideUp();
-			jQuery('#school').css('height', newHeight);
-			jQuery('#calendar').weekCalendar('resizeCalendar', newHeight);
+	$('a#pull-tab').toggle( function() {
+			$(this).html('More');
+			var newHeight = $(window).height() - $('#controls').outerHeight();
+			$('#header').slideUp();
+			$('#school').css('height', newHeight);
+			$('#calendar').weekCalendar('resizeCalendar', newHeight);
 		}, function() {
-			jQuery(this).html('Less');
-			var newHeight = jQuery(window).height() - jQuery('#header').outerHeight() - jQuery('#controls').outerHeight();
-			jQuery('#header').slideDown();
-			jQuery('#school').css('height', newHeight);
-			jQuery('#calendar').weekCalendar('resizeCalendar', newHeight);
+			$(this).html('Less');
+			var newHeight = $(window).height() - $('#header').outerHeight() - $('#controls').outerHeight();
+			$('#header').slideDown();
+			$('#school').css('height', newHeight);
+			$('#calendar').weekCalendar('resizeCalendar', newHeight);
 		}
 	);
 	
