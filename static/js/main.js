2076d6734cf670750e5c39ec94c352387f430b6a
Ryan Hsu <ryanlives@gmail.com>
2011-02-09 20:55:47 -0800

    refactor: created Utils object and added getRandColorPair()

--- static/js/main.js
+++ static/js/main.js
@@ -19,32 +19,34 @@ function genID() {
 	return ID_COUNT;
 }
 
-function genColorPair() {
-	var palette = [
-		{color: '#C4A883', borderColor: '#B08B59'},
-		{color: '#A7A77D', borderColor: '#898951'},
-		{color: '#85AAA5', borderColor: '#5C8D87'},
-		{color: '#94A2BE', borderColor: '#5C8D87'},
-		{color: '#8997A5', borderColor: '#627487'},
-		{color: '#A992A9', borderColor: '#8C6D8C'},
-		{color: '#A88383', borderColor: '#A87070'},
-		{color: '#E6804D', borderColor: '#DD5511'},
-		{color: '#F2A640', borderColor: '#EE8800'},
-		{color: '#E0C240', borderColor: '#D6AE00'},
-		{color: '#BFBF4D', borderColor: '#AAAA11'},
-		{color: '#8CBF40', borderColor: '#66AA00'},
-		{color: '#4CB052', borderColor: '#109618'},
-		{color: '#65AD89', borderColor: '#329262'},
-		{color: '#59BFB3', borderColor: '#22AA99'},
-		{color: '#668CD9', borderColor: '#3366CC'},
-		{color: '#668CB3', borderColor: '#336699'},
-		{color: '#8C66D9', borderColor: '#6633CC'},
-		{color: '#B373B3', borderColor: '#994499'},
-		{color: '#E67399', borderColor: '#DD4477'},
-		{color: '#D96666', borderColor: '#CC3333'}
-	];
-	
-	return palette[Math.floor(Math.random() * palette.length)];
+Utils = {
+	'getRandColorPair': function() {
+		var palette = [
+			{color: '#C4A883', borderColor: '#B08B59'},
+			{color: '#A7A77D', borderColor: '#898951'},
+			{color: '#85AAA5', borderColor: '#5C8D87'},
+			{color: '#94A2BE', borderColor: '#5C8D87'},
+			{color: '#8997A5', borderColor: '#627487'},
+			{color: '#A992A9', borderColor: '#8C6D8C'},
+			{color: '#A88383', borderColor: '#A87070'},
+			{color: '#E6804D', borderColor: '#DD5511'},
+			{color: '#F2A640', borderColor: '#EE8800'},
+			{color: '#E0C240', borderColor: '#D6AE00'},
+			{color: '#BFBF4D', borderColor: '#AAAA11'},
+			{color: '#8CBF40', borderColor: '#66AA00'},
+			{color: '#4CB052', borderColor: '#109618'},
+			{color: '#65AD89', borderColor: '#329262'},
+			{color: '#59BFB3', borderColor: '#22AA99'},
+			{color: '#668CD9', borderColor: '#3366CC'},
+			{color: '#668CB3', borderColor: '#336699'},
+			{color: '#8C66D9', borderColor: '#6633CC'},
+			{color: '#B373B3', borderColor: '#994499'},
+			{color: '#E67399', borderColor: '#DD4477'},
+			{color: '#D96666', borderColor: '#CC3333'}
+		]
+		
+		return palette[Math.floor(Math.random() * palette.length)];
+	}
 }
 
 function isDuplicateCourse(courseString) {
@@ -266,7 +268,7 @@ jQuery(document).ready(function() {
 			);
 			
 			//generate color for the event
-			var colorPairing = genColorPair();
+			var colorPairing = Utils.getRandColorPair();
 			
 			//create the course events
 			for(i in calEvents) {
