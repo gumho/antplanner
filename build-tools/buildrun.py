7c57cb592f34dc550c0f790aadab321f5de5d25b
Ryan Hsu <ryanlives@gmail.com>
2011-02-09 20:47:31 -0800

    fixed paths in buildrun.py

--- build-tools/buildrun.py
+++ build-tools/buildrun.py
@@ -1,4 +1,4 @@
 import os
-os.system('python ../web/template.py --compile templates/')
+os.system('python ../web/template.py --compile ../templates/')
 #os.system('dev_appserver.py --clear_datastore .')
-os.system('dev_appserver.py ../')
+os.system('/usr/local/bin/dev_appserver.py ../')
