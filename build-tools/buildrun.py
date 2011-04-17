b3f359cf506ac8e19e3099e2a85005a2912cc3f8
Ryan Hsu <ryanlives@gmail.com>
2011-04-16 21:49:47 -0700

    updated paths, added clear datastore flag

--- build-tools/buildrun.py
+++ build-tools/buildrun.py
@@ -1,4 +1,3 @@
 import os
 os.system('python ../web/template.py --compile ../templates/')
-#os.system('dev_appserver.py --clear_datastore .')
-os.system('/usr/local/bin/dev_appserver.py ../')
+os.system('/usr/local/bin/dev_appserver.py --clear_datastore ../')
